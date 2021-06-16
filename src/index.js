8e11ae1fdfc8316fda2a916a86ce88fc2c9f884a
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-16 13:34:48 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
