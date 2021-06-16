aae7bc690481592beb099d78fb6c5c9cbddd2955
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-16 13:35:20 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h1>Home!</h1>
+  <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
     </div>
   );
 };
